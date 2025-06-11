57def1cc54f3dc51ec99b1aa428d9868076ba874
OthavioLuis <othaviorodriguesf@gmail.com>
2025-06-10 22:50:26 -0300

    Fazer os icones aparecerem dps do client entender qual tema o usuário está usando

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -1,20 +1,29 @@
 'use client'
 
-import * as React from 'react'
+import { useState, useEffect } from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+    const { setTheme, theme } = useTheme()
 
-  return (
-    <button
-      className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors h-10 w-10"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-    >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </button>
-  )
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if(!mounted) {
+        return null
+    }
+
+    return (
+        <button
+            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors h-10 w-10 text-foreground"
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            aria-label='Toggle theme'
+        >
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </button>
+    )
 }
